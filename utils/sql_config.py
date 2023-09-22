4bc27e3903d061fed73f48820fc4549c58eec94d
Vialon17 <89980242+Vialon17@users.noreply.github.com>
2023-09-22 16:25:14 +0800

    Update sql_config.py

--- utils/sql_config.py
+++ utils/sql_config.py
@@ -1,17 +1,5 @@
-import pymysql
+import pymysql, os
 from dbutils.pooled_db import PooledDB
-import yaml
-import os
-
-def load_yaml(
-        file_name: str, 
-        encoding = "utf-8", 
-        yaml_loader = yaml.FullLoader) -> dict:
-    if not os.path.isfile(file_name):
-        raise ValueError("Invalid file name.")
-    with open(file_name, 'r', encoding = encoding) as f:
-        re_dict = yaml.load(f, Loader = yaml_loader)
-    return re_dict
 
 def get_pool(connection = pymysql, maxconn = 20, **config_para) -> PooledDB:
     config_para = load_yaml("config.yaml").get("mysql") if len(config_para) == 0 else config_para
