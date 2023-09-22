c53bbaf1034d05e0dd2fa6a6441a4b62ddcb3023
Vialon17 <89980242+Vialon17@users.noreply.github.com>
2023-09-22 16:25:59 +0800

    Update read_write.py

--- func/read_write.py
+++ func/read_write.py
@@ -1,4 +1,14 @@
-import PyPDF2
+import PyPDF2, yaml
+
+
+def read_yaml(file_name: str, encoding = "utf-8") -> dict:
+    with open(file_name, "r", encoding = encoding) as f:
+        data = yaml.load(f, Loader = yaml.FullLoader)
+    return data
+
+def write_yaml(file_name: str, obj: dict, encoding = "utf-8") -> None:
+    with open(file_name, "w", encoding = encoding) as f:
+        yaml.dump(obj, f, allow_unicode = True, indent = 4)
 
 def extract_pdf_text(pdf_file_path: str) -> str:
     '''
