6611fcc99f47678832c973343d493e6c1e5ca886
Vialon17 <vialon109@gmail.com>
2023-10-18 21:07:59 +0800

    update some func.

--- utils/file.py
+++ utils/file.py
@@ -1,6 +1,18 @@
 import os, yaml, json, pickle
 from typing import Literal
 from traceback import print_exc
+import PyPDF2
+
+def extract_pdf_text(pdf_file_path: str) -> str:
+    '''
+    Extract all text data from pdf file
+    '''
+    text = ""
+    with open(pdf_file_path, 'rb') as pdf_file:
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        for page_number in range(len(pdf_reader.pages)):
+            text += pdf_reader.pages[page_number].extract_text()
+    return text
 
 class File:
 
